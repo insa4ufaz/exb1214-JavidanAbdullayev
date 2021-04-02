7d43335a4517e070503900bd76499ddc180c05bf
javidan_abdullayev <>
2021-04-02 13:04:15 +0000

    test

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,23 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
+       int length = s.length()
+       for(int i=0; i<length/2; i++){
+         if(s.charAt(i) != s.charAt(length-1-i)){
+           return false;
+         }
+       }
         return true;
     }
+
+    public static void main(String[]args){
+      if(is_palindrome(args[0])){
+        System.out.println("is Palindrome");
+      }
+      else{
+        System.out.println("not Palindrome");
+
+      }
+    }
 }
+        
